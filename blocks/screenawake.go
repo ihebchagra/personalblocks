94f3758f735971f9e43d9aa81db85e469088cbf8
ihebchagra <ihebchagra@gmail.com>
2020-02-23 12:38:15 +0100

    fix screenawake.go

--- blocks/screenawake.go
+++ blocks/screenawake.go
@@ -8,16 +8,12 @@ import (
 
 
 func Screenawake(button string) {
-	if _, err := os.Stat("/tmp/lockscreen.lock"); ! os.IsNotExist(err) {
-		fmt.Println("")
-	}
-
-
 	if (button == "1") || (button == "3") {
 		if err := os.Remove("/tmp/lockscreen.lock"); err == nil {
 			cmd := exec.Command("notify-send"," Automatic screenlock is now enabled").Run()
 			_ = cmd
 		}
+	} else if _, err := os.Stat("/tmp/lockscreen.lock"); ! os.IsNotExist(err) {
+		fmt.Println("")
 	}
-
 }
